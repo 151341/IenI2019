374c219a5daf0ea313aa6a6f0b2d7aac2483d8c7
151341 <151341@csgaugustinus.nl>
2019-11-20 10:21:01 +0000

    vdzdc

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -8,15 +8,18 @@ function setup() {
 function draw() {
   // groene cirkel zonder rand
   
+  noStroke();
   fill('green');
-  ellipse(225,225,425);
+  ellipse(225,225,400);
   fill('white');
- 
-  
+
+  noStroke();
+  fill('white');
+  ellipse(225,225,300);
   // witte rechthoek met rode rand
   
   stroke('red');
   fill('white');
   strokeWeight(10);
-  rect(650,100,250,300);  
+  rect(125,125,200,200);
 }
