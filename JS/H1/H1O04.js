07e0c9b3e2f9033b0bc7c0b9980144d1a58a0925
151341 <151341@csgaugustinus.nl>
2019-11-25 12:32:01 +0000

    dsafsdaf

--- JS/H1/H1O04.js
+++ JS/H1/H1O04.js
@@ -7,38 +7,43 @@ function setup() {
 
 function draw() {
   // teken de grond
- 
-  
-  noStroke();
-  fill('red');
-  rect(120,280,40,50);
 
   noStroke();
   fill('wheat');
   rect(0,350,450,100);
   // teken het huis
 
-  noStroke();
-  fill('red');
-  rect(120,280,40,50);
-
   strokeWeight(5);
   stroke('darkgrey');
   fill('lightgray');
   rect(100,280,100,100);
+
+  noStroke();
   fill('gray');
+  rect(120,330,30,50);
+
+  strokeWeight(5);
+  stroke('darkgray');
+  fill('gray');
+  triangle(100,275,150,200,200,275);
   // VERVANG DEZE REGEL door een regel die het dak tekent
   // teken de deur
-  noStroke();
+
   // PAS ONDERSTAANDE REGEL AAN zodat de deur op de juiste plaats staat
-  rect(10,10,30,50);
+ noStroke();
+ fill('yellow');
+ ellipse(360,100,150);
   // teken de maan
   fill('khaki');
   // VERVANG DEZE REGEL door een regel die de maan tekent met een diameter van 150 en 25 vanaf de rand
   // teken de boom
+  noStroke();
   fill('sienna');
   rect(300,250,40,130);
+
+  noStroke();
   fill('olive');
+  ellipse(320,180,130,180);
   // VERVANG DEZE REGEL door een regel die het groen van de boom tekent met behulp van ellipse
   // Als je een cirkel wilt tekenen met diameter 300 kun je ellipse(0,0,300) gebruiken.
   // Als je een ellipse wilt maken met breedte 300 en hoogte 500, dan moet je een parameter toevoegen: ellipse(0,0,300,500).
