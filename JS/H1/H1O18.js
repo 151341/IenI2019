ce0393444d74ca3fcfabe5a4e78a5d0f88350913
151341 <151341@csgaugustinus.nl>
2019-12-12 17:08:40 +0000

    45678765

--- JS/H1/H1O18.js
+++ JS/H1/H1O18.js
@@ -1,29 +1,28 @@
 var xPositie = 0;
 var yPositie = 30;
-var s = 1;
+var schaal = 0.3;
 
 function setup() {
   canvas = createCanvas(1000,300);
   canvas.parent('processing');
   frameRate(15);
   colorMode(RGB,255,255,255,1);
-  background(0,0,75,0.1);
+  background(0,0,75,1);
 }
 
 function draw() {
   background(0,0,75,0.1);
   tekenSter(xPositie,yPositie,schaal);
-  yPositie+=1;
-  xPositie+=5;
-  s += 0,05;
+
+  schaal += 0.05;
 }
 
 function tekenSter(x,y,s) {
   push();
-  scale(1);
+  scale(schaal);
   strokeWeight(2);
   stroke('white');
-  fill('khaki');
+  fill('red');
   translate(x,y);
   triangle(0,0,80,0,40,65);
   translate(0,40);
