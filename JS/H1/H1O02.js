9eb57cb5f9ba06dd3204a4543827000d76f39bab
151341 <151341@csgaugustinus.nl>
2020-01-13 10:29:18 +0000

    YTRYR

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,15 +1,36 @@
+var getal = 5;
+
 function setup() {
-  canvas = createCanvas(450,450);
+  canvas = createCanvas(850,450);
   background('darkred');
   canvas.parent('processing');
-  noLoop();
+  
 }
 
+
+
 function draw() {
-  noStroke();
-  fill('pink');
-  triangle(450,0,0,225,450,450);
-  
-  fill('purple');
-  triangle(0,450,0,0,450,225);
+  background('yellow');
+text('aantal =' + getal,10,20);
+
+
+for (var n = 0;n <= getal;n++) {
+rel(n);
+translate(100,0);
+
+   if (keyIsDown(RIGHT_ARROW)){
+    getal++;
+   }
+
+  if (keyIsDown(LEFT_ARROW) && getal > 0){
+    getal--;
+    }
+  }
+}
+
+
+
+function rel(x) {
+    fill('red');
+    ellipse(50,150,90);
 }
